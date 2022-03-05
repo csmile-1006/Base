6fd16f94b73bb492f023637b3628ab60b459710c
csmile-1006 <doli1573@kaist.ac.kr>
2022-03-05 21:02:52 +0900

    change install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 ln dot.gitconfig ~/.gitconfig
 ln dot.tmux.conf ~/.tmux.conf
+cp -f dot.bashrc ~/.bashrc
 # install pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
+mkdir ~/.vim/colors
+cp colors/gruvbox.vim ~/.vim/colors/gruvbox.vim
 ln dot.vimrc ~/.vimrc
-source ~/.bashrc
